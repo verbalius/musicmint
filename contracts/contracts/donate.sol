088de7130511faef50aa435dbc7d63a57b87afc3
Yurii Horbal <yuriihorbal@gmail.com>
2023-05-12 22:39:15 +0100

    contract getLastDonations

--- contracts/contracts/donate.sol
+++ contracts/contracts/donate.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract DonationContract {
@@ -6,21 +7,35 @@ contract DonationContract {
         uint256 amount;
         uint256 timestamp;
     }
-    
+
     Donation[] public donations;
-    
+
     event DonationReceived(address indexed donor, uint256 amount, uint256 timestamp);
-    
-    function donate() external payable {
+
+    function donate() public payable {
         require(msg.value > 0, "Donation amount must be greater than 0");
-        
+
         Donation memory newDonation;
         newDonation.donor = msg.sender;
         newDonation.amount = msg.value;
         newDonation.timestamp = block.timestamp;
-        
+
         donations.push(newDonation);
-        
+
         emit DonationReceived(newDonation.donor, newDonation.amount, newDonation.timestamp);
+    }
+
+    function getLastDonations() public view returns (Donation[] memory) {
+        uint256 count = donations.length;
+        uint256 start = count > 5 ? count - 5 : 0;
+        uint256 resultLength = count > 5 ? 5 : count;
+
+        Donation[] memory lastDonations = new Donation[](resultLength);
+
+        for (uint256 i = 0; i < resultLength; i++) {
+            lastDonations[i] = donations[start + i];
+        }
+
+        return lastDonations;
     }
 }
