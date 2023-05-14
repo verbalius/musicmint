210dc3ed8464f2de0e7683f05494a8ddd1e577bf
Valentyn <spotichka1@gmail.com>
2023-05-14 02:29:42 +0100

    constants

--- frontend/src/constants /api.ts
+++ frontend/src/constants /api.ts
@@ -1,1 +1,2 @@
-export const GET_STREAMS_URL = "http://localhost:1985/api/v1/streams/";
+export const BASE_URL = "http://localhost:1985";
+export const GET_STREAMS_URL = `${BASE_URL}/api/v1/streams/`;
